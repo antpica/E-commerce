a6bd603a42b614aa342b6fe8b7f97359faacdfaf
antpica <antpica24@yahoo.com>
2023-07-05 15:51:24 -0400

    finished

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # E-commerce
+
+## Description
+
+The purpose of this project is to improve the code by creating a backend using node.js, express.js server, and a mySQL database. Since this is a backend-only application, insomnia was used to make requests to the APIs and demonstrate their functionality.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Built Using](#built-using)
+- [Links](#links)
+
+
+## Installation
+
+1. Clone/download git repository.
+2. Run "npm i" command.
+3. Run "npm run seed" command.
+4. Run "npm start" command to run the application.
+
+## Built Using
+
+- Node.js
+- Express.js
+- MySQL2
+- Sequelize
+- Insomnia
+
+## Links
+
+- Video Submission Link: https://drive.google.com/file/d/1l6HG2dnBGeyh4v1-Pd5R2kh4Sswvkt_0/view
+- GitHub Repository Link: https://github.com/antpica/E-commerce
---
+++ routes/api/product-routes.js
@@ -0,0 +1,143 @@
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
+
+// The `/api/products` endpoint
+
+// get all products
+router.get('/', async(req, res) => {
+  // find all products
+  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Tag, through: ProductTag, as: 'tags' }, {model: Category}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one product
+router.get('/:id', async(req, res) => {
+  // find a single product by its `id`
+  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // JOIN with products, using the ProductTag through table
+      include: [{ model: Tag, through: ProductTag, as: 'tags' }, {model: Category}]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create new product
+router.post('/', async (req, res) => {
+  /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
+  Product.create(req.body)
+    .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
+    })
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+// update product
+router.put('/:id', (req, res) => {
+  // update product data 
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(async () => {
+      // check if array of products tagIds were sent
+      if (req.body.tagIds && req.body.tagIds.length) {
+        // Find all of the tags for the product
+        const productTags = await ProductTag.findAll({
+          where: { product_id: req.params.id }
+        });
+        // transform the product tag data
+        const productTagIds = productTags.map(({ tag_id }) => tag_id);
+
+        /* When updating product tags, the array sent over may contain new tag_ids 
+           and it may also be void of tag_ids that used to be there.
+           We have to account for both situations:  */
+
+        // create filtered list of new tag_ids
+        const newProductTags = req.body.tagIds
+          // filter where the tag_ids coming over are not already in the current tag list
+          .filter((tag_id) => !productTagIds.includes(tag_id))
+          .map((tag_id) => {
+            return {
+              product_id: req.params.id,
+              tag_id,
+            };
+          });
+        // figure out which ones to remove
+        const productTagsToRemove = productTags
+          // filter where the current tag list has tags that are not present in the new tag list coming over
+          .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+          .map(({ id }) => id);
+        // run both actions asynchronously
+        Promise.all([
+          ProductTag.destroy({ where: { id: productTagsToRemove } }),
+          ProductTag.bulkCreate(newProductTags),
+        ]);
+        return res.json("Successfully updated with updated product tags")
+      }
+      return res.json("Successfully updated (no product tags included)");
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', async(req, res) => {
+  // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
